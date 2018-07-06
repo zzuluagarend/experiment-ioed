21a9f29e7ca37ae419bf2d1c1e0d31e924490d74
[zzuluagarend] <zzuluagar@gmail.com>
2018-07-06 13:54:23 +0200

    without randomization

--- views/views.js
+++ views/views.js
@@ -67,7 +67,8 @@ var main = {
 	trials : 1,
 	
     render : function(CT) {
-		var questions = _.shuffle(exp.trial_info.main_trials[CT].questions)
+		var questions = exp.trial_info.main_trials[CT].questions;
+		// var questions = _.shuffle(exp.trial_info.main_trials[CT].questions);
 
 		// fill variables in view-template
          var viewTemplate = $('#main-view').html();
@@ -87,81 +88,53 @@ var main = {
 			 q12 : questions[11],
 			 
          }));
-		var q1;
-		var q2;
-		var q3;
-		var q4;
-		var q5;
-		var q6;
-		var q7;
-		var q8;
-		var q9;
-		var q10;
-		var q11;
-		var q12;
 		
 		$('input:radio').click(function() {
-		q1 = questions[0]
 		answer1 = $('input:radio[name=q1]:checked').val();
-		q2 = questions[1]
 		answer2 = $('input:radio[name=q2]:checked').val();
-		q3 = questions[2]
 		answer3 = $('input:radio[name=q3]:checked').val();
-		q4 = questions[3]
 		answer4 = $('input:radio[name=q4]:checked').val();
-		q5 = questions[4]
 		answer5 = $('input:radio[name=q5]:checked').val();
-		q6 = questions[5]
 		answer6 = $('input:radio[name=q6]:checked').val();
-		q7 = questions[6]
 		answer7 = $('input:radio[name=q7]:checked').val();
-		q8 = questions[7]
 		answer8 = $('input:radio[name=q8]:checked').val();
-		q9 = questions[8]
 		answer9 = $('input:radio[name=q9]:checked').val();
-		q10 = questions[9]
 		answer10 = $('input:radio[name=q10]:checked').val();
-		q11 = questions[10]
 		answer11 = $('input:radio[name=q11]:checked').val();
-		q12 = questions[11]
 		answer12 = $('input:radio[name=q12]:checked').val();
 		
 				
 		// provide plus minus arrows next to each answer to indicate deviation from average
-	});
-		
-		// update the progress bar
-        var filled = CT * (180 / exp.views_seq[exp.currentViewCounter].trials);
-        $('#filled').css('width', filled);
+	});		
 
         // event listener for buttons; when an input is selected, the response
 		// and additional information are stored in exp.trial_info
 		$('#next').on('click', function(e) {
             trial_data = {
                 trial_type: "main",
-                q1: q1,
+                q1: questions[0],
 				answer1: answer1,
-				q2: q2,
+				q2: questions[1],
 				answer2: answer2,
-				q3: q3,
+				q3: questions[2],
 				answer3: answer3,
-				q4: q4,
+				q4: questions[3],
 				answer4: answer4,
-				q5: q5,
+				q5: questions[4],
 				answer5: answer5,
-				q6: q6,
+				q6: questions[5],
 				answer6: answer6,
-				q7: q7,
+				q7: questions[6],
 				answer7: answer7,
-				q8: q8,
+				q8: questions[7],
 				answer8: answer8,
-				q9: q9,
+				q9: questions[8],
 				answer9: answer9,
-				q10: q10,
+				q10: questions[9],
 				answer10: answer10,
-				q11: q11,
+				q11: questions[10],
 				answer11: answer11,
-				q12: q12,
+				q12: questions[11],
 				answer12: answer12,			
                        
             };
